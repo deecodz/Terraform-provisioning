036a1ff9e81aec360f017d98f014978ac16034c6
deecodz <okoyechidera26@gmail.com>
2023-05-21 13:02:50 +0000

    My security group file for inbound rules

--- modules/security-groups/security-groups.tf
+++ modules/security-groups/security-groups.tf
@@ -1,3 +1,4 @@
+
 variable "security_group_name" {
   description = "my-security-group"
   type        = string
@@ -13,65 +14,89 @@ variable "final-proj-vpc" {
   type        = string
 }
 
+variable "inbound_rules" {
+  description = "List of inbound rules"
+  type        = list(object({
+    name        = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+}))
+  default = [
+    {
+      name         = "Grafana"
+      from_port    = 3000
+      to_port      = 3000
+      protocol     = "tcp"
+      cidr_blocks  = ["0.0.0.0/0"]
+    },
+    {
+      name         = "Node_Exporter"
+      from_port    = 9100
+      to_port      = 9100
+      protocol     = "tcp"
+      cidr_blocks  = ["0.0.0.0/0"]
+    },
+    {
+      name         = "SSH"
+      from_port    = 22
+      to_port      = 22
+      protocol     = "tcp"
+      cidr_blocks  = ["0.0.0.0/0"]
+    },
+    {
+      name         = "Http"
+      from_port    = 80
+      to_port      = 80
+      protocol     = "tcp"
+      cidr_blocks  = ["0.0.0.0/0"]
+    },
+    {
+      name         = "jenkins"
+      from_port    = 8080
+      to_port      = 8080
+      protocol     = "tcp"
+      cidr_blocks  = ["0.0.0.0/0"]
+    },
+    {
+      name         = "prometheus"
+      from_port    = 9090
+      to_port      = 9090
+      protocol     = "tcp"
+      cidr_blocks  = ["0.0.0.0/0"]
+    },
+    {
+      name         = "Flask"
+      from_port    = 5000
+      to_port      = 5000
+      protocol     = "tcp"
+      cidr_blocks  = ["0.0.0.0/0"]
+    },
+    {
+      name         = "Https"
+      from_port    = 443
+      to_port      = 443
+      protocol     = "tcp"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+  ]
+}
+
 resource "aws_security_group" "my-security-group" {
   name        = var.security_group_name
   description = var.security_group_description
   vpc_id      = var.final-proj-vpc
 
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 9100
-    to_port     = 9100
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.inbound_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      description = ingress.value.name
+    }
   }
 }
 
